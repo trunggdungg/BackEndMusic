d7c945ace615eca202ea0269cdc83f6327b0b488
trunggdungg <20210531@eaut.edu.vn>
2026-01-16 13:48:09 +0700

    add method to find favorites by user ID with sorting

--- src/main/java/com/example/musicbackend/repository/FavoriteRepository.java
+++ src/main/java/com/example/musicbackend/repository/FavoriteRepository.java
@@ -1,9 +1,15 @@
 package com.example.musicbackend.repository;
 
 import com.example.musicbackend.entity.Favorite;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FavoriteRepository extends JpaRepository<Favorite, Integer> {
+
+  List<Favorite> findByUserId(Integer id, Sort createdAt);
 }
