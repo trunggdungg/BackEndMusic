642f8cde93eb6efe660bdde9d40526b5d7863f40
trunggdungg <20210531@eaut.edu.vn>
2026-01-16 13:48:15 +0700

    add method to retrieve favorites by user ID with user existence check

--- src/main/java/com/example/musicbackend/service/FavoriteService.java
+++ src/main/java/com/example/musicbackend/service/FavoriteService.java
@@ -1,10 +1,35 @@
 package com.example.musicbackend.service;
 
+import com.example.musicbackend.entity.Favorite;
+import com.example.musicbackend.model.respone.UserNotFoundException;
+import com.example.musicbackend.repository.FavoriteRepository;
+import com.example.musicbackend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 
 public class FavoriteService {
+    @Autowired
+    private FavoriteRepository favoriteRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<Favorite> getFavoriteByUserId(Integer userId) {
+        if (!userRepository.existsById(userId)) {
+            throw new UserNotFoundException(userId);
+        }
+
+        return favoriteRepository.findByUserId(
+                userId,
+                Sort.by(Sort.Direction.DESC, "createdAt")
+        );
+    }
 }
