a2bcc0697bda45178989f4ff7633922dcaabd139
trunggdungg <20210531@eaut.edu.vn>
2026-01-16 13:47:11 +0700

    add endpoint to retrieve songs by artist

--- src/main/java/com/example/musicbackend/rest/ArtistApi.java
+++ src/main/java/com/example/musicbackend/rest/ArtistApi.java
@@ -1,9 +1,12 @@
 package com.example.musicbackend.rest;
 
 import com.example.musicbackend.entity.Artist;
+import com.example.musicbackend.entity.Song;
 import com.example.musicbackend.service.ArtistService;
+import com.example.musicbackend.service.SongService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +17,15 @@ import java.util.List;
 public class ArtistApi {
     @Autowired
     private ArtistService artistService;
-
+    @Autowired
+    private SongService songService;
     @GetMapping
     public List<Artist> getAllArtists() {
         return artistService.getAllArtists();
+    }
+
+    @GetMapping("/{artistId}/songs")
+    public List<Song> getSongsByArtist(@PathVariable Integer artistId) {
+        return songService.getSongsByArtist(artistId);
     }
 }
